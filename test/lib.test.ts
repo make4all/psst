7ac191415566a814c3c985df417259904115046b
pelikhan <jhalleux@microsoft.com>
2021-09-15 20:34:45 -0700

    adding bundling

--- test/lib.test.ts
+++ test/lib.test.ts
@@ -1,10 +1,10 @@
-import { hello } from "../src/lib";
-import 'mocha';
-import {expect} from 'chai';
+import { hello } from '../src/sonification'
+import 'mocha'
+import { expect } from 'chai'
 
-describe('hello world test',() => {
-  it('should return hello world', () => {
-      const r = hello()
-      expect(r).to.equal('hello world!')
-  });  
-});
+describe('hello world test', () => {
+    it('should return hello world', () => {
+        const r = hello()
+        expect(r).to.equal('hello world!')
+    })
+})
