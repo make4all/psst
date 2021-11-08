f3ba6e2351e3fdfaadad3536559efdddc67533e4
Venkatesh Potluri <vpotluri@cs.washington.edu>
2021-11-08 16:10:10 +0400

    replaced Monaco editor with simple textbox.

--- src/Demo.tsx
+++ src/Demo.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { TextareaHTMLAttributes, useState } from 'react'
 import { hello, playTone } from './sonification'
 import Editor from '@monaco-editor/react'
 import { editor } from 'monaco-editor'
@@ -7,9 +7,9 @@ import { array } from 'vega'
 export const Demo = () => {
     const [editorText, setEditorText] = useState('1,20,500,340,400,20,30,1000,800')
 
-    const handleEditorChange = (value?:string) => {
-      if (value)
-        setEditorText(value)
+    const handleEditorChange: React.ChangeEventHandler<HTMLTextAreaElement> | undefined= (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+      if (event.target.value)
+        setEditorText(event.target.value)
     }
 
     const playButtonHandeler = () => {
@@ -23,7 +23,8 @@ export const Demo = () => {
 
     return (<div>
         <h1> basic sonification demo</h1> {hello()}
-        <Editor height="90vh" defaultLanguage="javascript" defaultValue={editorText} onChange={handleEditorChange} />
+        <textarea value={editorText}onChange={handleEditorChange}/> 
+        {/* <Editor height="90vh" defaultLanguage="javascript" defaultValue={editorText} onChange={handleEditorChange} /> */}
         <button onClick={playButtonHandeler}>play</button>
     </div>)
 }
