b8fb0dfe650764246a2d2f4ff4d9693aa2b838e4
James Devine <t-jamdev@microsoft.com>
2021-11-08 10:56:37 +0000

    fix demo
    state/interface related functions should be scoped within the handler
    (not globally)

--- src/Demo.tsx
+++ src/Demo.tsx
@@ -1,30 +1,29 @@
-import React from "react";
-import { hello, playTone } from "./sonification";
-import Editor from "@monaco-editor/react";
-import { editor } from "monaco-editor";
-import { array } from "vega";
-let editorText:string = "1,20,500,340,400,20,30,1000,800";
-function handleEditorChange(value: any, event: any) {
-  // console.log("here is the current model value:", value);
-  editorText = value;
-}
+import React, { useState } from 'react'
+import { hello, playTone } from './sonification'
+import Editor from '@monaco-editor/react'
+import { editor } from 'monaco-editor'
+import { array } from 'vega'
 
-function playButtonHandeler(){
-  var data: number[] = [];
-  var dataText:string[] = editorText.split(",");
-  for(let i=0;i<dataText.length;i++){
-    data.push(parseInt(dataText[i]));
-  }
-  playTone(data);
-} 
-export const Demo = () =>
-  <div>
-    <h1> basic sonification demo</h1>    {hello()}
-    <Editor
-     height="90vh"
-     defaultLanguage="javascript"
-     defaultValue={editorText}
-     onChange={handleEditorChange}
-   />
-    <button onClick= {playButtonHandeler}>play</button>
-  </div>
+export const Demo = () => {
+    const [editorText, setEditorText] = useState('1,20,500,340,400,20,30,1000,800')
+
+    const handleEditorChange = (value?:string) => {
+      if (value)
+        setEditorText(value)
+    }
+
+    const playButtonHandeler = () => {
+        var data: number[] = []
+        var dataText: string[] = editorText.split(',')
+        for (let i = 0; i < dataText.length; i++) {
+            data.push(parseInt(dataText[i]))
+        }
+        playTone(data)
+    }
+
+    return (<div>
+        <h1> basic sonification demo</h1> {hello()}
+        <Editor height="90vh" defaultLanguage="javascript" defaultValue={editorText} onChange={handleEditorChange} />
+        <button onClick={playButtonHandeler}>play</button>
+    </div>)
+}
