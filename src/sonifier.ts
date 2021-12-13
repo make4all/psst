a6bc640dfe1442fb966726c9bbb0d8937818fc48
Venkatesh Potluri <vpotluri@cs.washington.edu>
2021-12-13 15:40:31 +0400

    contains errors: re-factored to support updated scheduling #60 for simple sonifications.

--- src/sonifier.ts
+++ src/sonifier.ts
@@ -1,5 +1,5 @@
 // import { SonificationLevel } from './constents';
-import { AudioQueue } from './sonificationUtils'
+import { AudioQueue} from './sonificationUtils'
 import * as d3 from 'd3'
 import { PlaybackState, SonificationLevel, SonificationType } from './constents'
 //move enums to constants.ts . Currently seeing runtime JS error saying that the enum is not exported from constants.ts so placing them here to move forward with building.
@@ -18,23 +18,48 @@ export class Sonifier {
     protected didNodesFinishPlaying: boolean
     private _playbackState: PlaybackState
     private previousPlaybackState: PlaybackState
+    private _data: number[]
+private     previousPriority: SonificationLevel
+    public get data(): number[] {
+        return this._data
+    }
+    public set data(value: number[]) {
+        this._data = value
+    }
     public get playbackState(): PlaybackState {
         return this._playbackState
     }
     public onPlaybackStateChanged?: (state: PlaybackState) => void
+    private _pointQueue: [{}]
+    public get pointQueue(): [{}] {
+        return this._pointQueue
+    }
+    private currentDataPointIndex:number;
+    private scheduleAheadTime:number;
+    private nextPointTime:number;
+    private timerID:number | undefined;
+    
     private constructor() {
         // super()
         this.audioCtx = new AudioContext() // works without needing additional libraries. need to check this when we move away from react as it is currently pulling from react-dom.d.ts.
-        this.startTime = this.audioCtx.currentTime
-        this.endTime = this.startTime
+        this.startTime = this.audioCtx.currentTime;
+        this.nextPointTime = this.startTime;
+        this.endTime = this.startTime;
+        
         this.audioQueue = new AudioQueue()
         this.isStreamInProgress = false
         this.previousFrequencyOfset = 50
         this.pointSonificationLength = 0.3
-        this.priority = SonificationLevel.polite
+        this.previousPriority = SonificationLevel.polite;
+        this.priority = SonificationLevel.polite;  
         this._playbackState = PlaybackState.Stopped
         this.previousPlaybackState = PlaybackState.Stopped
         this.didNodesFinishPlaying = true
+        this._pointQueue = [{}];
+        this.scheduleAheadTime = 2*this.pointSonificationLength; // we could compute this by computing the stream rate for data streams.
+        this.currentDataPointIndex = 0;
+        this.timerID = undefined;
+        this._data = [];
     }
     public static getSonifierInstance(): Sonifier {
         if (!Sonifier.sonifierInstance) {
@@ -44,25 +69,35 @@ export class Sonifier {
         return Sonifier.sonifierInstance
     }
 
+    private scheduler() {
+        while(this.nextPointTime < this.audioCtx.currentTime+this.scheduleAheadTime && this.currentDataPointIndex < this.data.length)
+        {
+            this.pointQueue.push({pointIndex:this.currentDataPointIndex, dataPoint:this.data[this.currentDataPointIndex],time:this.nextPointTime});
+            this.sonifyPoint(this.data[this.currentDataPointIndex],this.nextPointTime);
+            this.nextPointTime += this.pointSonificationLength;
+            this.currentDataPointIndex+=1;
+
+        }
+        // move this part to a web worker.
+        this.timerID = window.setTimeout(this.scheduler,this.scheduleAheadTime);
+    }
+
     public playSimpleTone(dummyData: number[]): void {
         console.log('playTone: sonifying data', dummyData)
-
-        let pointSonificationLength: number = 0.3
-        let previousFrequencyOfset: number = 50
-        let startTime: number = this.audioCtx.currentTime
-
+this.data = dummyData;
         let frequencyExtent = [16, 1e3]
-        let dataExtent = d3.extent(dummyData)
+        let dataExtent = d3.extent(this.data)
 
         let frequencyScale = d3.scaleLinear().domain(dataExtent).range(frequencyExtent)
 
-        for (let i = 0; i < dummyData.length; i++) {
-            let scaledDataPoint = frequencyScale(dummyData[i])
+        for (let i = 0; i < this.data.length; i++) {
+            let scaledDataPoint = frequencyScale(this.data[i])
+            this.data[i] = scaledDataPoint;
 
-            this.sonifyPoint(scaledDataPoint)
-            this.isStreamInProgress = true
+
         }
-        this.isStreamInProgress = false
+        this.scheduler();
+        this.isStreamInProgress = true
     }
 
     public playHighlightPointsWithNoise(dummyData: number[], highlightPoint: number): void {
@@ -102,31 +137,22 @@ export class Sonifier {
 
     private sonifyPoint(
         dataPoint: number,
-        priority: SonificationLevel = SonificationLevel.polite,
-        sonificationType: SonificationType = SonificationType.Tone,
+        pointTime:number,
+        sonificationLevel:SonificationLevel = SonificationLevel.polite,
+        sonificationType:SonificationType = SonificationType.Tone
     ) {
         console.log('in sonify point. datapoint:', dataPoint)
 
         console.log('isStreamInProgress', this.isStreamInProgress)
-        if (priority == SonificationLevel.rude && this.priority != SonificationLevel.rude) {
-            this.audioQueue.emptyAudioQueue()
-            this.startTime = this.audioCtx.currentTime
-            this.endTime = this.startTime
+        if (sonificationLevel == SonificationLevel.rude && this.previousPriority != SonificationLevel.rude) {
+            this.resetSonifier()
         }
         if (!this.isStreamInProgress) {
-            this.previousFrequencyOfset = 50
-            this.audioQueue.emptyAudioQueue()
+            this.resetSonifier();   
             this.isStreamInProgress = true
         }
-        if (this.audioCtx.currentTime < this.endTime) {
-            // method is called when a previous tone is still scheduled to play.
-            this.startTime = this.endTime
-        } else {
-            this.audioQueue.emptyAudioQueue()
-            this.startTime = this.audioCtx.currentTime
-        }
-        this.endTime = this.startTime + this.pointSonificationLength
-        this.priority = priority // to keep track of priority of previous point
+        
+        this.previousPriority = this.priority // to keep track of priority of previous point
         if (sonificationType == SonificationType.Tone) {
             this.scheduleOscilatorNode(dataPoint)
         } else if (sonificationType == SonificationType.Noise) {
@@ -139,16 +165,24 @@ export class Sonifier {
         }
 
         this.previousFrequencyOfset = dataPoint
+
+        
+    }
+    private resetSonifier() {
+        this.audioQueue.emptyAudioQueue()
+        this.startTime = this.audioCtx.currentTime
+        this.endTime = this.startTime
+        this.previousFrequencyOfset = 50;
     }
 
-    private scheduleOscilatorNode(dataPoint: number) {
+    private scheduleOscilatorNode(dataPoint: number,pointTime:number) {
         let osc = this.audioCtx.createOscillator()
         osc.frequency.value = this.previousFrequencyOfset
-        osc.frequency.linearRampToValueAtTime(dataPoint, this.startTime + this.pointSonificationLength)
+        osc.frequency.linearRampToValueAtTime(dataPoint, pointTime  + this.pointSonificationLength)
         osc.onended = () => this.handelOnEnded()
         osc.connect(this.audioCtx.destination)
-        osc.start(this.startTime)
-        osc.stop(this.endTime)
+        osc.start(pointTime)
+        osc.stop(pointTime+this.pointSonificationLength)
         this.audioQueue.enqueue(osc)
         if (this.playbackState == PlaybackState.Stopped) {
             this._playbackState = PlaybackState.Playing
