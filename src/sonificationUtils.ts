1bd7050c8cd1848ac28525c40a8ca662e0c8cbfb
Venkatesh Potluri <vpotluri@cs.washington.edu>
2021-12-13 11:48:44 +0400

    introducing queue to manage datapoints for scheduling as mentioned in #60

--- src/sonificationUtils.ts
+++ src/sonificationUtils.ts
@@ -27,3 +27,32 @@ export class AudioQueue {
         while (this.enqueuePointer !== this.dequeuePointer) this.dequeue()
     }
 }
+
+export class PointQueue {
+    private storage: { [index: number]: number }
+    private enqueuePointer: number
+    private dequeuePointer: number
+
+    public constructor() {
+        this.dequeuePointer = 0
+        this.enqueuePointer = 0
+        this.storage = Object.create(null)
+    }
+    public enqueue(item: number) {
+        this.storage[this.enqueuePointer] = item
+        this.enqueuePointer++
+    }
+
+    public dequeue(): number | undefined {
+        if (this.dequeuePointer !== this.enqueuePointer) {
+            const dequeuedData = this.storage[this.dequeuePointer]
+            delete this.storage[this.dequeuePointer];
+            this.dequeuePointer++;
+            return dequeuedData;
+        }
+        return undefined
+    }
+    public emptyPointQueue(): void {
+        while (this.enqueuePointer !== this.dequeuePointer) this.dequeue()
+    }
+}
