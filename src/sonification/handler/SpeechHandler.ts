84f59f91b0b8254d35d689d288538041e2d467d7
Nora Morsi <nora.m.morsi@gmail.com>
2022-02-03 17:21:57 -0800

    updating naming format

--- src/sonification/handler/SpeechHandler.ts
+++ src/sonification/handler/SpeechHandler.ts
@@ -2,7 +2,7 @@ import { Speech } from '../output/Speech'
 import * as d3 from 'd3'
 import { Datum } from '../Datum'
 import { DataSink } from '../DataSink'
-import { DataHandler } from '../handler/DataHandler'
+import { DataHandler } from './DataHandler'
 const DEBUG=true;
 /**
  * A DataHandler that outputs a Datum as speech.
