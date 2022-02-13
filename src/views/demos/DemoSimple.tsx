d5b19ce0717bc4ccf7bca462e99621eb7cad2209
Venkatesh Potluri <vpotluri@cs.washington.edu>
2022-02-13 14:12:36 -0800

    added more testing for multiple streams. each stream now plays from a separate audio channel. please use headphones for testing.

--- src/views/demos/DemoSimple.tsx
+++ src/views/demos/DemoSimple.tsx
@@ -44,6 +44,29 @@ export class DemoSimple<DemoSimpleProps, DemoSimpleState>
     }
 
     /**
+     * Holder for the current DataSink object that starts after a delay
+     */
+     protected delaySink: DataSink | undefined
+     public getDelaySink() {
+        if(this.delaySink) return this.delaySink
+        else return this.initializeDelaySink()
+         }
+    initializeDelaySink() {
+        // SONIFICATION
+        debugStatic(SonificationLoggingLevel.DEBUG, `adding sink`)
+
+        this.delaySink = OutputEngine.getInstance().addSink('SimpleDemoDelaySink')
+
+        // debugStatic(SonificationLoggingLevel.DEBUG, `adding Handler`)
+
+        // this.sink?.addDataHandler(new NoteHandler())
+
+        debugStatic(SonificationLoggingLevel.DEBUG, `success initializing sink ${this.sink}`)
+
+        return this.delaySink
+    }
+
+    /**
      * @todo fix comments and debug
      * @param data Not sure what this is -- someone else wrote ti
      */
@@ -65,16 +88,21 @@ export class DemoSimple<DemoSimpleProps, DemoSimpleState>
      * @param data The data set to be played
      */
     public onPlay = (data: Array<number>) => {
-        debugStatic(SonificationLoggingLevel.DEBUG, `in onPlay ${this.sink} ${data}`)
+        debugStatic(SonificationLoggingLevel.DEBUG, `in onPlay ${this.sink}, ${this.delaySink} `)
         debugStatic(SonificationLoggingLevel.DEBUG, `adding sink`)
 
         if (this.sink == undefined) this.sink = this.initializeSink()
+        if(this.delaySink == undefined) this.delaySink = this.initializeDelaySink()
 
         let id = this.sink ? this.sink.id : 0
 
+        let delayID = this.delaySink ? this.delaySink.id : 1
+        let dataCopy = Object.assign([],data)
         let data$ = of(...data) //.slice(0, 8))
+        let delayData$ = of(...dataCopy) //.slice(0, 8))
+
         let timer$ = timer(0, 500).pipe(debug(SonificationLoggingLevel.DEBUG, 'point number'))
-        let source$ = zip(data$, timer$, (num, time) => new Datum(id, num)).pipe(delay(2000)).pipe(
+        let source$ = zip(data$, timer$, (num, time) => new Datum(id, num)).pipe(
             debug(SonificationLoggingLevel.DEBUG, 'point'),
         )
 
@@ -97,6 +125,29 @@ export class DemoSimple<DemoSimpleProps, DemoSimpleState>
             ],-1),
         ) // max
         debugStatic(SonificationLoggingLevel.DEBUG, `success`)
+
+        let delayTimer$ = timer(0, 250).pipe(debug(SonificationLoggingLevel.DEBUG, 'point number'))
+        let delaySource$ = zip(delayData$, delayTimer$, (num, time) => new Datum(delayID, num)).pipe(delay(1000)).pipe(
+            debug(SonificationLoggingLevel.DEBUG, 'delayPoint'),
+        )
+        OutputEngine.getInstance().setStream(delayID, delaySource$)
+
+        /// Make sure to delete the sink when the source is
+        delaySource$.subscribe({
+            complete: () => {
+                this.delaySink = undefined
+                //Demo.setState({ playbackLabel: "Play" })
+            },
+        })
+        
+
+        debugStatic(SonificationLoggingLevel.DEBUG, `adding Handler to ${this.delaySink}`)
+        this.delaySink?.addDataHandler(
+            new NoteHandler([
+                data.reduce((prev, curr) => (prev < curr ? prev : curr)), // min
+                data.reduce((prev, curr) => (prev > curr ? prev : curr)),
+            ],1),
+        ) // max
 
         console.log('sending play')
         // Change State
