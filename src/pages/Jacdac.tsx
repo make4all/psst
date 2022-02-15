f2d520bb3a7716ef7c5277f39906c25c10fb2eb5
Venkatesh Potluri <vpotluri@cs.washington.edu>
2022-02-15 13:40:17 -0800

    basic JacDac demo now works with stereo panning!

--- src/pages/Jacdac.tsx
+++ src/pages/Jacdac.tsx
@@ -12,8 +12,22 @@ import { bus } from '../bus'
 import { DataSink } from '../sonification/DataSink'
 import { NoteHandler } from '../sonification/handler/NoteHandler'
 import { OutputStateChange } from '../sonification/OutputConstants'
+import { filter, Observable, OperatorFunction, pipe, Subject, UnaryFunction } from 'rxjs'
+import { Datum } from '../sonification/Datum'
 
 const TONE_THROTTLE = 100
+
+/**
+ * helper function to filter out null values from subjects, and create an observable<Datum> for the sink to subscribe.
+ * Source: https://stackoverflow.com/questions/57999777/filter-undefined-from-rxjs-observable
+ * @returns observable <datum>
+ */
+
+ function filterNullish<T>(): UnaryFunction<Observable<T | null | undefined>, Observable<T>> {
+    return pipe(
+      filter(x => x != null) as OperatorFunction<T | null |  undefined, T>
+    );
+  }
 
 function ConnectButton() {
     const bus = useBus()
@@ -21,6 +35,8 @@ function ConnectButton() {
     const services = useServices({ serviceClass: SRV_ACCELEROMETER })
     const [streaming, setStreaming] = useState(false)
     const [sink, setSink] = useState<DataSink>()
+    const [xAxisStream, setXAxisStream] = useState<Subject<Datum>>()
+    const [yAxisStream, setYAxisStream] = useState<Subject<Datum>>()
 
     useEffect(() => {
         if (!inIFrame() && window.location.hash === '#dbg') startDevTools()
@@ -39,7 +55,8 @@ function ConnectButton() {
                 if (!streaming || !sink) return
                 const [x, y, z] = accelService.readingRegister.unpackedValue
                 console.log('vpotluri: calling PushPoint.')
-                OutputEngine.getInstance().pushPoint(x, sink.id)
+                if(sink && xAxisStream) xAxisStream.next(new Datum(sink.id,x))
+                // OutputEngine.getInstance().pushPoint(x, sink.id)
             }, TONE_THROTTLE),
         )
 
@@ -58,14 +75,27 @@ function ConnectButton() {
     }
 
     const handleStartStreaming = () => {
+        let sinkID:number = 0;
         let src = sink
         if (!streaming) {
             if (!src) {
                 src = OutputEngine.getInstance().addSink('jacdac demo')
-                src.addDataHandler(new NoteHandler())
+                src.addDataHandler(new NoteHandler([-1,1],-1))
                 // src.addDataHandler(new FilterRangeHandler(new NoiseSonify(), [-1, 0]))
                 // dummy stats. Do we know the min and max for accelerometer?
+                sinkID = src.id;
                 setSink(src)
+
+
+            }
+
+            let sourceX = xAxisStream;
+            if(!sourceX)
+            {
+                sourceX = new Subject<Datum>();
+                setXAxisStream(sourceX)
+                OutputEngine.getInstance().setStream(sinkID,sourceX.pipe(filterNullish()))
+
             }
 
             OutputEngine.getInstance().next(OutputStateChange.Play)
