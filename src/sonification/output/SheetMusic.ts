d24dc279248ad4194ddc538391b6ff5413be9d71
Nora Morsi <nora.m.morsi@gmail.com>
2022-03-09 14:48:28 -0800

    circle on page

--- src/sonification/output/SheetMusic.ts
+++ src/sonification/output/SheetMusic.ts
@@ -10,6 +10,8 @@ const DEBUG = false;
  */
 export class SheetMusic extends DatumOutput {
 
+    // choose to either save last or first
+
     // the range of values expected
     private domain : [Statistic, Statistic]
     // the range of values to be outputted
@@ -32,15 +34,20 @@ export class SheetMusic extends DatumOutput {
         // parsed svg into a document, can now append to document
         const doc = parser.parseFromString(svg, "image/svg+xml")
         console.log("parsed svg", doc)
-        const myElement = document.getElementById('for-svg')!
-        if (doc && myElement) {
-            myElement.appendChild(doc.documentElement)
+        // spot in doc to place SVG
+        const forSVG = document.getElementById('for-svg')!
+        if (doc && forSVG) {
+            var svgElem = doc.querySelector('svg')!
+            var circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+            circle.setAttribute( 'cx', '122' );
+            circle.setAttribute( 'cy', '169' );
+            circle.setAttribute( 'r', '3' );
+            svgElem.appendChild(circle)
+            forSVG.appendChild(doc.documentElement)
         }
     }
 
     protected output(datum: Datum): void {
-        // try loading in file here?
-
         // convert value to closest note and add to list of values seen
         let idx : number = Math.round(this.convertToNote(datum.value))
         let note : string = Note[idx]
