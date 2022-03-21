11f117b1606d304171d10aaf90b0030e3adbf0ce
James Devine <t-jamdev@microsoft.com>
2022-03-21 19:31:43 +0000

    add mkc project for micro:bit

---
+++ makecode-microbit/main.ts
@@ -0,0 +1,37 @@
+namespace servers {
+    /**
+     * Start all microbit servers
+     */
+    //% blockId=jacdac_microbit_start_all
+    //% block="start all jacdac servers"
+    export function startAll() {
+        jacdac.startServer()
+        if (!jacdac.isSimulator()) {
+            // servers.buttonA.start()
+            // servers.buttonB.start()
+            // servers.buttonAB.start()
+            // servers.buttonLogo.start()
+            // servers.touchP0.start()
+            // servers.touchP1.start()
+            // servers.touchP2.start()
+            // servers.screenServer.start()
+            servers.soundLevelServer.start()
+            servers.lightLevelServer.start()
+            servers.temperatureServer.start()
+            servers.accelerometerServer.start()
+            // servers.buzzerServer.start()
+            // servers.soundPlayerServer.start()
+            // calibration "kills" jacdac
+            // microbit.compassServer.start()
+            servers.radioServer.start()
+        }
+
+        if (jacdac.checkProxy()) jacdac.proxyFinalize()
+        else {
+            control.runInBackground(function () {
+                pause(100)
+                basic.showIcon(IconNames.Happy)
+            })
+        }
+    }
+}
