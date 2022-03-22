649e859059817780a97e9d90d050c53d2e1d51bf
pelikhan <jhalleux@microsoft.com>
2022-03-21 20:31:20 -0700

    reduce amount of code

--- makecode-microbit/main.ts
+++ makecode-microbit/main.ts
@@ -1,37 +1,6 @@
-namespace servers {
-    /**
-     * Start all microbit servers
-     */
-    //% blockId=jacdac_microbit_start_all
-    //% block="start all jacdac servers"
-    export function startAll() {
-        jacdac.startServer()
-        if (!jacdac.isSimulator()) {
-            // servers.buttonA.start()
-            // servers.buttonB.start()
-            // servers.buttonAB.start()
-            // servers.buttonLogo.start()
-            // servers.touchP0.start()
-            // servers.touchP1.start()
-            // servers.touchP2.start()
-            // servers.screenServer.start()
-            servers.soundLevelServer.start()
-            servers.lightLevelServer.start()
-            servers.temperatureServer.start()
-            servers.accelerometerServer.start()
-            // servers.buzzerServer.start()
-            // servers.soundPlayerServer.start()
-            // calibration "kills" jacdac
-            // microbit.compassServer.start()
-            servers.radioServer.start()
-        }
-
-        if (jacdac.checkProxy()) jacdac.proxyFinalize()
-        else {
-            control.runInBackground(function () {
-                pause(100)
-                basic.showIcon(IconNames.Happy)
-            })
-        }
-    }
-}
+servers.start({
+    soundLevel: true,
+    lightLevel: true,
+    temperature: true,
+    accelerometer: true
+})
