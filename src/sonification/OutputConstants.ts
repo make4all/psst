80a44fda53ff34f79e2d317587b5f612401e2792
noramorsi <nora.m.morsi@gmail.com>
2022-04-01 22:52:03 -0700

    cleaning up constants

--- src/sonification/OutputConstants.ts
+++ src/sonification/OutputConstants.ts
@@ -79,32 +79,3 @@ const pianoKeys = new Map<string, [number, boolean]>([
 export function getPianoKeys() {
     return pianoKeys;
 }
-
-
-const keyFreq = new Map<string, number>([
-    ['Digit1', 65.41], // C2
-    ['Digit2', 73.42], // D2
-    ['Digit3', 82.41], // E2
-    ['Digit4', 87.31], // F2
-    ['Digit5', 98.00], // G2
-    ['Digit6', 110.00], // A2
-    ['Digit7', 123.47], // B2
-    ['KeyQ', 130.81], // C3
-    ['KeyW', 146.83], // D3
-    ['KeyE', 164.81], // E3
-    ['KeyR', 174.61], // F3
-    ['KeyT', 196.00], // G3
-    ['KeyY', 220.00], // A3
-    ['KeyU', 246.94], // B3
-    ['KeyA', 261.63], // C4
-    ['KeyS', 293.66], // D4
-    ['KeyD', 329.63], // E4
-    ['KeyF', 349.23], // F4
-    ['KeyG', 392.00], // G4
-    ['KeyH', 440.00], // A4
-    ['KeyJ', 493.88], // B4
-])
-
-export function getKeyFreq() {
-    return keyFreq;
-}
