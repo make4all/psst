448d89eefe77440c9c33de39c5601981b046d5f2
Peli de Halleux <pelikhan@users.noreply.github.com>
2022-10-18 16:47:33 +0000

    updated index

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -10,13 +10,27 @@ const Index: FC = (props) => {
     ]
 
     return (
-        <ul>
-            {opts.map((opt) => (
-                <li key={opt.url}>
-                    <Link to={opt.url}>{opt.name}</Link>
+        <>
+            <h1>Physical Computing Streaming Sensor Tooltip (PSST)</h1>
+            <p>
+                The Physical computing Streaming Sensor Sonification Toolkit (PSSST) is designed to simplify the
+                authoring of sonification of live sensor data for the purposes of understanding, testing, and debugging
+                sensors. The toolkit is designed more generally to support authoring across different types of streaming
+                data, but has prioritized features that will specifically be helpful when dealing with multiple sensor
+                data streams.
+            </p>
+            <h2>Tools</h2>
+            <ul>
+                <li>
+                    <a href="https://github.com/make4all/psst">GitHub repository</a>
                 </li>
-            ))}
-        </ul>
+                {opts.map((opt) => (
+                    <li key={opt.url}>
+                        <Link to={opt.url}>{opt.name}</Link>
+                    </li>
+                ))}
+            </ul>
+        </>
     )
 }
 
